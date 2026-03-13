974013b729bee52d51c9507b07c72dcf5f7f6b1c
Bruno Vinicius Siqueira Santos <bruno10916@gmail.com>
2026-03-13 18:02:57 -0300

    fix: movendo config de seed para o prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,9 @@ import 'dotenv/config';
 import { defineConfig, env } from '@prisma/config';
 
 export default defineConfig({
+  migrations: {
+    seed: 'npx ts-node prisma/seed.ts',
+  },
   datasource: {
     url: env('DATABASE_URL'),
   },
